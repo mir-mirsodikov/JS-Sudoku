6e4bc8d42d61631d262ffcc759a43eb55a372750
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2020-04-06 03:59:43 -0500

    added difficulty buttons

--- game.js
+++ game.js
@@ -4,35 +4,35 @@
   * All the game logistics are stored here
 */
 
-//let gamePuzzle = new Puzzle; // initialize gamePuzzle object of the Puzzle class
 let table = "<table>"; //initialize a table tag
 let q = 1; // a counter for quadrants
-let puzzle; //puzzlesAll[Math.floor(Math.random() * puzzlesAll.length)] // retrieve random puzzle from the array
-//let puzzle = gamePuzzle.createPuzzle();/*puzzlesAll[Math.floor(Math.random() * puzzlesAll.length)]*/
-//let solution = solution3;
+let puzzle;
+var puzzleString;
+var difficulty;
+var solution;
 
-window.onload = init;
+var difficultyButtons = document.getElementById("difficulty").getElementsByTagName("td"); //get all the difficulty buttons in one
+let cellStyle = document.getElementById("highlight"); // create a variable that holds the style tag
+
+//Assign the getDifficulty function to all the difficulty buttons
+for (let i = 0; i < difficultyButtons.length; i++) {
+  difficultyButtons[i].onclick = getDifficulty;
+}
+
+emptyDisplay();
 
 //-------------------------------------------------------------------------------
 
 function init() {
+    // puzzle = sudoku.board_string_to_grid(sudoku.generate("easy"));
     // c = row and r = column. I don't know how that happened
-    puzzle = sudoku.board_string_to_grid(sudoku.generate("easy"));
-    console.log(puzzle);
+    table = "<table>";
     for (let c = 0; c < puzzle.length; c++) // a for loop for a 2D array - this is the rows
     {
         table += "<tr id='row-" + (c + 1) + "'>"; //set up the rows
         for (let r = 0; r < puzzle[0].length; r++) // this is the column
         {
-            /* Quadrant
-                1 2 3  |
-                4 5 6  |
-                7 8 9  |
-              ----------
-            */
-
-
-            //trying to sort out the quadrants
+            //sort out the quadrants
             if (c < 3 && r < 3) q = 1;
             else if (c < 3 && r < 6) q = 2;
             else if (c < 3 && r < 9) q = 3;
@@ -70,6 +70,7 @@ function init() {
         numButtons += "<td id='num-" + i + "'>" + i + "</td>";
     }
 
+    //close off the table
     numButtons += "</tr></table>";
 
     //add the table to the HTML file
@@ -104,19 +105,41 @@ function init() {
 
 //-------------------------------------------------------------------------------
 
-// create a variable that holds the style tag
-let cellStyle = document.getElementById("highlight");
+//Configure the difficulty buttons
+function getDifficulty(e) {
+  //turn off the click procedure after one of the buttons have been clicked
+  for (let i = 0; i < difficultyButtons.length; i++) {
+    difficultyButtons[i].onclick = null;
+  }
+  difficulty = e.target.id;
+  document.getElementById(difficulty).id = difficulty + "-selected"; //add a special tag to keep it highlighted
+  puzzleString = sudoku.generate(difficulty); //generate the puzzle with selected difficulty
+  puzzle = sudoku.board_string_to_grid(puzzleString); //turn the puzzle into a 2D array
+  solution = sudoku.board_string_to_grid(sudoku.solve(puzzleString)); //generate a solution for the selected puzzle
+  init(); //call the main function
+}
+
+//Display an empty table that will be filled after difficulty is selected
+function emptyDisplay() {
+  for (let c = 0; c < 9; c++)
+  {
+      table += "<tr>"; //set up the rows
+      for (let r = 0; r < 9; r++) // this is the column
+      {
+          table += "<td>";
+          table += "</td>"
+      }
+      table += "</tr>"
+  }
+  table += "</table>";
+
+  //add the table to the html file
+  document.getElementById("board").innerHTML = table;
+}
 
 //-------------------------------------------------------------------------------
 
 function highlight(e) { //highlight the entire 3x3 block and the row and col
-    /*
-         1 2 3
-       1 * * *
-       2 * * *
-       3 * * *
-
-    */
 
     // row-xcol-yquad-znum-c
     // gather the id assigned to each table cell and then organize them in appropriate variables
@@ -135,17 +158,17 @@ function highlight(e) { //highlight the entire 3x3 block and the row and col
     s += "td#" + cellID + ", input#" + cellID + "{background-color: #BBDEFB;}"; //highluight the cell
     s += "td[id*='" + cellNum + "'], input[id*='" + cellNum + "'] {background-color: #CBDBED;}"; //highlight all cells with same num
     cellStyle.innerHTML = s;
-    console.log(cellRow + " " + cellCol + " " + quadrant + " " + cellNum);
+    //console.log(cellRow + " " + cellCol + " " + quadrant + " " + cellNum);
 }
 
 //-------------------------------------------------------------------------------
 
+//Highlight all the numbers selected by the table on the bottom
 function showNums(e) {
     let cellID = e.target.id;
     let x = "";
-    x += "td[id*='" + cellID + "'], input[id*='" + cellID + "'] {background-color: #CBDBED;}";
+    x += "td[id*='" + cellID + "'], input[id*='" + cellID + "'] {background-color: #CBDBED;}"; //highlight all the same numbers
     cellStyle.innerHTML = x;
-    console.log(cellID);
 }
 
 //-------------------------------------------------------------------------------
@@ -163,19 +186,31 @@ function changeInputID(e) {
 //-------------------------------------------------------------------------------
 
 function addNumToPuzzle(e) {
-    let cellID = e.target.id;
-    let cellRow = parseInt(cellID.substring(4, 5), 10) - 1;
-    let cellCol = parseInt(cellID.substring(9, 10), 10) - 1;
-    let selectedCell = document.getElementById(cellID);
-    let num = document.getElementById(cellID).value;
-    puzzle[cellRow][cellCol] = parseInt(document.getElementById(cellID).value, 10);
-    selectedCell.id = cellID + "num-" + num;
-    highlight(e);
+    let cellID = e.target.id; //get the cell id
+    let cellRow = parseInt(cellID.substring(4, 5), 10) - 1; //get the cell row
+    let cellCol = parseInt(cellID.substring(9, 10), 10) - 1; // get the cell column
+    let selectedCell = document.getElementById(cellID); //get the specific cell selected
+    let num; //the number being input
+    //if the selected cell is empty then go ahead and input
+    if (puzzle[cellRow][cellCol] == ".") {
+      num = selectedCell.value; //get the number from the cell
+      selectedCell.id = cellID + "num-" + num; //add the number to the id
+    }
+    //if there is already a number there then change the number and id
+    else {
+      num = String.fromCharCode(e.keyCode); //get number from the keyCode
+      selectedCell.value = num; //change the value of the cell
+      selectedCell.id = cellID.substring(0, cellID.length - 1) + num; //change the number in the id
+    }
+    puzzle[cellRow][cellCol] = num; // add the number to the array
+    highlight(e); //highlight the board
+
     //checkPuzzle(cellRow, cellCol, selectedCell);
-    console.log(selectedCell.id);
-    console.log(cellRow + " " + cellCol);
-    console.log(puzzle);
+    //console.log(selectedCell.id);
+    //console.log(cellRow + " " + cellCol);
+    //console.log(puzzle);
 }
+
 /*
 function checkPuzzle(row, col, cell) {
     if (puzzle[row][col] == solution3[row][col])
@@ -189,14 +224,5 @@ function checkPuzzle(row, col, cell) {
         cell.classList.add("wrong");
         console.log(cell.class);
     }
-}
-
-function dispNums(puzzle)
-{
-    for (let i = 0; i < puzzle[4].length; i++)
-        console.log(puzzle[4][i]);
-
-    for (let i = 0; i < puzzle.length; i++)
-        console.log(puzzle[i][0]);
 }
 */
